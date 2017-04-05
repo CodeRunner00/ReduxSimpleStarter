50f2c27506e980d79105e5497a13c357a3041941
Eric Forsell <efor202@gmail.com>
2017-04-04 20:19:19 -0400

    Refactor ES6

--- src/index.js
+++ src/index.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-const API_KEY='AIzaSyAD4tR7VBZV29-e7edIb7eoCxQLucHckXE';
+import YTSearch from 'youtube-api-search';
+
 
 import SearchBar from './components/search_bar';
-//  Create a new component.  This component should produce some HTML.
 
-const App = () => {
-  return (
-   <div>
-    <SearchBar />
-  </div>
-  );
+const API_KEY='AIzaSyAD4tR7VBZV29-e7edIb7eoCxQLucHckXE';
+
+
+
+//  Create a new component.  This component should produce some HTML.
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });//{ videos: videos }
+      console.log(data);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 //Take this component's gneerated HTML and put it on the page (in the DOM)
